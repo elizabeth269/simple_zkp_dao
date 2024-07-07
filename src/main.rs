faa8cd73034fa1c3b9b69f9f7b1a8f865f9ffc58
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-07 19:09:39 +0100

    nae a few changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, Web};
+use actix_files as fs;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use uuid::Uuid;
 
 mod zkp; // this module contains ZKP-related functions
@@ -15,28 +16,29 @@ struct Proposal {
 
 #[derive(Debug, Clone)]
 struct AppState {
-    proposals: Mutex<Vec<Proposal>>,
+    proposals: Arc<Mutex<Vec<Proposal>>>,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_state = Web::Data::new(AppState {
-        proposals: Mutex::new(vec![]),
+    let app_state = web::Data::new(AppState {
+        proposals: Arc::new(Mutex::new(vec![])),
     });
 
     HttpServer::new(move || {
         App::new()
             .app_data(app_state.clone())
-            .route("/proposals", Web::post().to(create_proposal))
-            .route("/proposals", Web::get().to(get_proposals))
-            .route("/approve/{id}", Web::post().to(approve_proposal))
+            .route("/proposals", web::post().to(create_proposal))
+            .route("/proposals", web::get().to(get_proposals))
+            .route("/approve/{id}", web::post().to(approve_proposal))
+            .service(fs::Files::new("/", "./static").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
-async fn create_proposal(item: Web::Json<Proposal>, data: Web::Data<AppState>) -> impl Responder {
+async fn create_proposal(item: web::Json<Proposal>, data: web::Data<AppState>) -> impl Responder {
     let mut proposals = data.proposals.lock().unwrap();
     let new_proposal = Proposal {
         id: Uuid::new_v4(),
@@ -48,12 +50,12 @@ async fn create_proposal(item: Web::Json<Proposal>, data: Web::Data<AppState>) -
     HttpResponse::Created().json(new_proposal)
 }
 
-async fn get_proposals(data: Web::Data<AppState>) -> impl Responder {
+async fn get_proposals(data: web::Data<AppState>) -> impl Responder {
     let proposals = data.proposals.lock().unwrap();
     HttpResponse::Ok().json(proposals.clone())
 }
 
-async fn approve_proposal(path: Web::Path<Uuid>, data: Web::Data<AppState>) -> impl Responder {
+async fn approve_proposal(path: web::Path<Uuid>, data: web::Data<AppState>) -> impl Responder {
     let mut proposals = data.proposals.lock().unwrap();
     let id = path.into_inner();
     if let Some(proposal) = proposals.iter_mut().find(|p| p.id == id) {
