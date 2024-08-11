a6305ad079da4105b69eda613df2fa60ae775f07
Elizbeth <elizabethadebayo269@gmail.com>
2024-08-11 20:57:07 +0100

    reading through

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_files as fs;
+// use actix_files as fs;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
